91de0723ee001b5287ba0db0e71e883c49afbc39
Taylor.R <nguyenhieucoder47@gmail.com>
2023-05-16 23:08:45 +0700

    Fixed problem

---
+++ src/TaylorR/NoFarmlandDecay/Main.php
@@ -0,0 +1,27 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace TaylorR\NoFarmlandDecay;
+
+use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\event\Listener as Event;
+use pocketmine\event\entity\EntityTrampleFarmlandEvent;
+
+class Main extends Plugin implements Event{
+
+	public function onEnable() : void {
+        $this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onEntityTrample(EntityTrampleFarmlandEvent $event): void
+    {
+		$entity = $event->getEntity();
+        $config = $this->getConfig()->get("worlds", []);
+        $condition = empty($config) || in_array($entity->getWorld()->getDisplayName(), $config);
+		if ($condition) {
+			$event->cancel();
+		}
+	}
+}
