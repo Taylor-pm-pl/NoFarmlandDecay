5f2362ae4760c582dcd44375b9899395b9a63dee
Taylor.R <nguyenhieucoder47@gmail.com>
2023-06-02 08:01:01 +0700

    Support Pm5

--- src/YTBJero/NoFarmlandDecay/Main.php
+++ src/YTBJero/NoFarmlandDecay/Main.php
@@ -4,18 +4,27 @@ declare(strict_types=1);
 
 namespace YTBJero\NoFarmlandDecay;
 
+use pocketmine\event\EventPriority;
 use pocketmine\plugin\PluginBase as Plugin;
 use pocketmine\event\Listener as Event;
 use pocketmine\event\entity\EntityTrampleFarmlandEvent;
 
 class Main extends Plugin implements Event
 {
+    /**
+     * @throws \ReflectionException
+     */
     public function onEnable(): void
     {
         $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $onEntityTrample = $this->onEntityTrample(...);
+        $this->getServer()->getPluginManager()->registerEvent(EntityTrampleFarmlandEvent::class, $onEntityTrample, EventPriority::HIGHEST, $this);
     }
 
+    /**
+     * @param EntityTrampleFarmlandEvent $event
+     * @return void
+     */
     public function onEntityTrample(EntityTrampleFarmlandEvent $event): void
     {
         $entity = $event->getEntity();
