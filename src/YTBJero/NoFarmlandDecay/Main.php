ff13096bb4f4aede120d37b4ce9d750e04d93d60
Taylor.R <nguyenhieucoder47@gmail.com>
2023-06-08 17:52:53 +0700

    Fix issue #2

--- src/YTBJero/NoFarmlandDecay/Main.php
+++ src/YTBJero/NoFarmlandDecay/Main.php
@@ -4,32 +4,51 @@ declare(strict_types=1);
 
 namespace YTBJero\NoFarmlandDecay;
 
-use pocketmine\event\EventPriority;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\event\block\BlockUpdateEvent;
 use pocketmine\plugin\PluginBase as Plugin;
 use pocketmine\event\Listener as Event;
 use pocketmine\event\entity\EntityTrampleFarmlandEvent;
+use pocketmine\world\World;
 
-class Main extends Plugin implements Event
-{
-    /**
-     * @throws \ReflectionException
-     */
+class Main extends Plugin implements Event {
+
     public function onEnable(): void
     {
         $this->saveDefaultConfig();
-        $onEntityTrample = \Closure::fromCallable([$this, "onEntityTrample"]);
-        $this->getServer()->getPluginManager()->registerEvent(EntityTrampleFarmlandEvent::class, $onEntityTrample, EventPriority::HIGHEST, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     /**
      * @param EntityTrampleFarmlandEvent $event
      * @return void
      */
-    public function onEntityTrample(EntityTrampleFarmlandEvent $event): void
-    {
+    public function onEntityTrample(EntityTrampleFarmlandEvent $event): void {
         $entity = $event->getEntity();
+        $world = $entity->getWorld();
+        $this->onBreakFarmLand($world, $event);
+    }
+
+    /**
+     * @param BlockUpdateEvent $event
+     * @return void
+     */
+    public function onBlockUpdate(BlockUpdateEvent $event): void {
+        $block = $event->getBlock();
+        $world = $block->getPosition()->getWorld();
+        if ($block->getName() === VanillaBlocks::FARMLAND()->getName()) {
+            $this->onBreakFarmLand($world, $event);
+        }
+    }
+
+    /**
+     * @param World $world
+     * @param EntityTrampleFarmlandEvent|BlockUpdateEvent $event
+     * @return void
+     */
+    protected function onBreakFarmLand(World $world, EntityTrampleFarmlandEvent|BlockUpdateEvent $event): void {
         $config = $this->getConfig()->get("worlds", []);
-        $condition = empty($config) || in_array($entity->getWorld()->getDisplayName(), $config);
+        $condition = empty($config) || in_array($world->getDisplayName(), $config);
         if ($condition) {
             $event->cancel();
         }
